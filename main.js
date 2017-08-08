4941ae3d9c2ba197de7d4b51ba320b84466bec7a
Angela Vogler <angelavogler4@gmail.com>
2017-08-08 15:54:53 -0400

    added more styling need to fix search bar still

--- main.js
+++ main.js
@@ -4,12 +4,26 @@ function convertFromJson (recipes) {
 
 function displayRecipes (recipes) {
   console.log(recipes);
+  
+  // define variables
+  let userInput = document.querySelector("#search-term");
   let container = document.querySelector(".container");
   let recipeDisplay = '';
+
   // iterate through array of recipes
   for (let i = 0; i < recipes.results.length; i++) {
-  // define variables
+
+  // define variable to hold indexes
     let recipe = recipes.results[i];
+
+  // search bar
+    // need to look through every single recipes.results[i].ingredients created
+    // if (recipe.ingredients === userInput.toUpperCase())
+    // return those recipes
+    // else
+    // return no results found
+
+  // template literal HTML to show search results
     let recipeCode = `
     <div>
       <a href="${recipe.href}">
@@ -25,6 +39,6 @@ function displayRecipes (recipes) {
   container.innerHTML += recipeDisplay;
 }
 
-fetch ("http://recipepuppyproxy.herokuapp.com/api/?i=squash")
+fetch ("http://recipepuppyproxy.herokuapp.com/api/?i=chocolate")
   .then(convertFromJson)
   .then(displayRecipes);
